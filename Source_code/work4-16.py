9cf638e68e31240cd25f5f8c9ce3735c479bcbc4
wyh0106 <weiyhdyx@126.com>
2020-04-21 19:34:17 +0800

    format

--- Source_code/work4-16.py
+++ Source_code/work4-16.py
@@ -1,5 +1,37 @@
 
 
-from random import random
+import random
 
-print(random())
+# print(random.uniform(2.5, 17.5))
+
+# print(random.choice(['小丑', '疾风', '王牌']))
+
+# number = random.randint(1, 100)
+# label = True
+
+# for i in range(1,11):
+#     if i == 10:
+#         label = False
+
+#     Guess = input("Guess a number in range of 1-100\n")
+#     if int(Guess) > number:
+#         print("It's bigger!\n")
+#     elif int(Guess) < number:
+#         print("It's smaller!\n")
+#     else:
+#         print("Great! You Win!\n")
+#         break
+
+# if label == False:
+#     print("OH NO,YOU ARE LOSE!")
+
+# def fib(n):
+#     if 0 < n <= 2:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+
+# u = int(input())
+# print(fib(u))
+
+
