070dc280a7a6718c12aa780212dd12281deb2219
wyh0106 <weiyhdyx@126.com>
2020-05-21 20:48:41 +0800

    format

--- Source_code/tts.py
+++ Source_code/tts.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-labels='frogs','hogs','dogs','logs'
-sizes=15,20,45,10
-colors='yellowgreen','gold','lightskyblue','lightcoral'
-explode=0,0.1,0,0
-plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct='%1.1f%%',shadow=True,startangle=50)
+
+labels = 'frogs', 'hogs', 'dogs', 'logs'
+sizes = 30, 20, 40, 10
+colors = 'purple', 'gold', 'lightskyblue', 'lightcoral'
+explode = 0, 0.1, 0, 0
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=50)
 plt.axis('equal')
 plt.show()
